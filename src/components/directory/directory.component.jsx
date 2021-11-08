86e2e8d20a11c95c2a9a6ff3bb17f82fb5171a62
Kirti Soni <kitokirti94@gmail.com>
2021-11-08 10:47:28 +0530

    rectified the collection preview component for the shop page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SECTIONS_DATA from "./sections.data.js";
 
 import MenuItem from '../menu-item/menu-item.component'
 import './directory.styles.scss';
@@ -8,47 +9,14 @@ class Directory extends React.Component {
         super();
 
         this.state = {
-            seactions: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl: 'shop/hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: 'shop/jackets'
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,
-                    linkUrl: 'shop/sneakers'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: 'shop/womens'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: 'shop/mens'
-                }
-            ]
+            sections: SECTIONS_DATA
         }
     }
 
     render(){
         return (
             <div className='directory-menu'>
-                {this.state.seactions.map(({ title, imageUrl, id, size, linkUrl})=>(
+                {this.state.sections.map(({ title, imageUrl, id, size, linkUrl})=>(
                     <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>
                 ))}
             </div>
